53855e2aed0f72d9d988e8197b9d12db0bf8103f
konstantinpetkow <tinkoci@abv.bg>
2018-10-09 17:46:54 +0300

    Homework for Week1, ex.1/2

---
+++ AgeAfter10Years/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgeAfter10Years
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+           
+            
+            Console.WriteLine(myAgeAfter10);
+            Console.ReadLine();
+            int myAge = 19;
+            int myAgeAfter10 = myAge + 10;
+
+
+        }
+    }
+}
---
+++ HiProgram/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HiProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myName = "I am Кonstantin";
+            Console.WriteLine(myName);
+            Console.ReadLine();
+
+
+        }
+    }
+}
