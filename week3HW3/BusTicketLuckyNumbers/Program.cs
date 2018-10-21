38e673e5e01c1bf2e015c1c6369e0507fe3df130
konstantinpetkow <tinkoci@abv.bg>
2018-10-21 12:20:38 +0300

    Initial commit.

---
+++ week3HW3/BusTicketLuckyNumbers/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusTicketLuckyNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int luckyTicketsCount = 0;
+            int unluckyTicketsCount = 0;
+
+            for (int i = 0; i < 1000000; i++)
+            {
+                if ((i / 100000 + i % 100000 / 10000 + i % 100000 % 10000 / 1000) == (i % 100000 % 10000 % 1000 / 100 + i % 100000 % 10000 % 1000 % 100 / 10 + i % 10))
+                {
+                    Console.WriteLine(i);
+                    luckyTicketsCount++;
+                }
+                else
+                {
+                    unluckyTicketsCount++;
+                }
+            }
+
+            Console.WriteLine("Total lucky ticket numbers are: " + luckyTicketsCount);
+            Console.WriteLine("Total not lucky ticket numbers are: " + unluckyTicketsCount);
+            Console.WriteLine($"Total tickets existing are: {luckyTicketsCount + unluckyTicketsCount}");
+        }
+    }
+}
